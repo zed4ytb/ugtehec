4b4b8d963e87c4e9e3dda3928e949b6f631bd742
zed4ytb <105384555+zed4ytb@users.noreply.github.com>
2026-09-07 16:37:54 +0100

    Update database connection details in db.php

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'ugte_db'; // 3awed baddelha b esm el base de données mte3k ken tbeddel
-$username = 'root';
-$password = '';
+$host = 'sql113.infinityfree.com'; // Baddel b-Host mte3ak
+$dbname = 'if0_42847852_ugtehec';   // Baddel b-Database Name mte3ak
+$username = 'if0_42847852';        // Baddel b-Username mte3ak
+$password = 'ziedugte';       // Baddel b-Password mte3ak
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+} catch (PDOException $e) {
+    die("Database Connection Error: " . $e->getMessage());
 }
 ?>
